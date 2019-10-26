d9612c06e65d4332b70569476c821dd4267f5c57
nurzatEjat <nurzatejat@live.com>
2019-10-26 15:46:41 -0400

    Nur

--- src/test/java/jqueryTest/Interaction.java
+++ src/test/java/jqueryTest/Interaction.java
@@ -1,5 +1,41 @@
 package jqueryTest;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
 public class Interaction {
 
+	public WebDriver driver;
+	
+	@Test
+	public void draggableTest() throws InterruptedException {
+		System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+		driver = new ChromeDriver();
+		
+		driver.get("https://jqueryui.com");
+		
+		driver.findElement(By.linkText("Draggable")).click();
+		
+		//wait   
+		//Implicite
+		//Explicite
+		//Fulent wait
+		//Thread.sleep
+		Thread.sleep(3*1000);
+		//Exception -> two type
+			//Runtime Exception
+			//Compiltime Exception
+		driver.switchTo().frame(0);
+		Actions move = new Actions(driver); 
+		WebElement dragBox = driver.findElement(By.id("draggable"));
+		move.moveToElement(dragBox, 200, -300).build().perform();
+		Thread.sleep(3000);
+		driver.switchTo().defaultContent();
+
+		driver.quit();
+	}
 }
